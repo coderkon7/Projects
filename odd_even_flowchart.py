28877fe57f927761279a2e301fcf4f298aaab50b
coderkon7 <jaydentiger77@gmail.com>
2025-07-30 18:26:03 -0500

    Odd Even Flowchart

---
+++ odd_even_flowchart.py
@@ -0,0 +1,6 @@
+while 1 == 1:
+    number = int(input("Enter the number"))
+    if number % 2 == 0:
+        print(number, "is an even number.")
+    elif number % 2 != 0:
+        print(number, "is an odd number")
