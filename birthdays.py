f7c64fbe425eba6b353a6dbd9d408a4f49fdf80f
coderkon7 <jaydentiger77@gmail.com>
2025-07-31 14:39:17 -0500

    Commit

---
+++ birthdays.py
@@ -0,0 +1,18 @@
+steve = "January 5th, 2015"
+olivia = "March 16th, 2013"
+sam = "July 23rd, 2016"
+charlotte = "August 30th, 2014"
+
+print("These are the birthdays of Raj's 5 friends:")
+print("Steve's birthday is", steve, "\nOlivia's birthday is", olivia, "\nSam's birthday is", sam, "\nCharlotte's birthday is", charlotte)
+
+
+
+
+
+month = "April"
+day = "14"
+year = "2014"
+
+print("This is Raj's birthday")
+print(month, day, ",", year)
