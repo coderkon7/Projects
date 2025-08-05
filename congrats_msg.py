c88534f5897c4559bf7fd35b535b69fbde5e59fc
coderkon7 <jaydentiger77@gmail.com>
2025-08-05 11:09:07 -0500

    Commit

---
+++ congrats_msg.py
@@ -0,0 +1,11 @@
+msg = input("Enter your message:").upper()
+print(msg)
+
+
+
+# just for fun
+# makes it look like congratulations banner, or just makes it stand out a bit more
+opening_msg_symbols = "!!! ==== "
+closing_msg_symbols = " ==== !!!"
+msg_2 = input("Enter your 2nd message:").upper()
+print(opening_msg_symbols, msg_2, closing_msg_symbols)
