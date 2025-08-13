ba123f78aee98b34ec4b09758bc0d3d14db5ac10
coderkon7 <jaydentiger77@gmail.com>
2025-08-13 11:05:10 -0500

    Commit

--- square_root.py
+++ square_root.py
@@ -1,20 +1,3 @@
-# if input_num//x and input_num//y
-# x (first number) = 10 and y (second number) = 10
-#  if x and y both equal 10 and when multiplied they equal the user input (input_num)...
-# it could then tell the user that there is a valid square root for that number, and tell the user what that number is
-
-# if number times 2 equals input_num, then it's found the square root
-
-'''while sqrt != inputNum:
-    sqrt *= 2
-    if sqrt == inputNum:
-        print(sqrt, "is the square root of ", inputNum)
-    if sqrt != inputNum:
-        sqrt //= 2
-        sqrt += 1
-        print("Haven't found the square root of that number yet")'''
-
-
 inputNum= int(input("Enter the number you wish to find the square root of: "))
 
 sqrt = 1
