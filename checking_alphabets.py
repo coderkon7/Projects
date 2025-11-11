1aefaa6daa52b8a3180240ae9dbddaed978d1b3f
coderkon7 <jaydentiger77@gmail.com>
2025-11-11 09:46:11 -0600

    Commit

---
+++ checking_alphabets.py
@@ -0,0 +1,59 @@
+
+
+userinput = input("Please type what you would like to check is a letter/alphabet.").lower()
+
+if userinput == "a":
+    print("That is the letter 'A'. It is in the alphabet.")
+elif userinput == "b":
+    print("That is the letter 'B'. It is in the alphabet.")
+elif userinput == "c":
+    print("That is the letter 'C'. It is in the alphabet.")
+elif userinput == "d":
+    print("That is the letter 'D'. It is in the alphabet.")
+elif userinput == "e":
+    print("That is the letter 'E'. It is in the alphabet.")
+elif userinput == "f":
+    print("That is the letter 'F'. It is in the alphabet.")
+elif userinput == "g":
+    print("That is the letter 'G'. It is in the alphabet.")
+elif userinput == "h":
+    print("That is the letter 'H'. It is in the alphabet.")
+elif userinput == "i":
+    print("That is the letter 'I'. It is in the alphabet.")
+elif userinput == "j":
+    print("That is the letter 'J'. It is in the alphabet.")
+elif userinput == "k":
+    print("That is the letter 'K'. It is in the alphabet.")
+elif userinput == "l":
+    print("That is the letter 'L'. It is in the alphabet.")
+elif userinput == "m":
+    print("That is the letter 'M'. It is in the alphabet.")
+elif userinput == "n":
+    print("That is the letter 'N'. It is in the alphabet.")
+elif userinput == "o":
+    print("That is the letter 'O'. It is in the alphabet.")
+elif userinput == "p":
+    print("That is the letter 'P'. It is in the alphabet.")
+elif userinput == "q":
+    print("That is the letter 'Q'. It is in the alphabet.")
+elif userinput == "r":
+    print("That is the letter 'R'. It is in the alphabet.")
+elif userinput == "s":
+    print("That is the letter 'S'. It is in the alphabet.")
+elif userinput == "t":
+    print("That is the letter 'T'. It is in the alphabet.")
+elif userinput == "u":
+    print("That is the letter 'U'. It is in the alphabet.")
+elif userinput == "v":
+    print("That is the letter 'V'. It is in the alphabet.")
+elif userinput == "w":
+    print("That is the letter 'W'. It is in the alphabet.")
+elif userinput == "x":
+    print("That is the letter 'X'. It is in the alphabet.")
+elif userinput == "y":
+    print("That is the letter 'Y'. It is in the alphabet.")
+elif userinput == "z":
+    print("That is the letter 'Z'. It is in the alphabet.")
+
+else:
+    print("You typed", userinput, ", which is not a known letter in the alphabet.")
