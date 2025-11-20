e883ac8d7058dcda6fdf785067785eca9cfea783
coderkon7 <jaydentiger77@gmail.com>
2025-11-19 18:11:11 -0600

    Commit

---
+++ ASCII_value.py
@@ -0,0 +1,3 @@
+userinput = input("Type the character, and the program will tell you that character's ASCII value.")
+
+print("The ASCII value for the letter '", userinput, "' is: ", ord(userinput))
