59c914b20c7f2f175702aa7caf38584872f8e593
coderkon7 <jaydentiger77@gmail.com>
2025-12-03 09:25:34 -0600

    Commit

---
+++ number_swap.py
@@ -0,0 +1,12 @@
+num1 = int(input("Enter the first integer: "))
+num2 = int(input("Enter the second integer: "))
+num3 = int(input("Enter the third integer: "))
+
+
+print("Your current integers are: ", "\nFirst number: ", num1, "\nSecond number: ", num2, "\nThird number: ", num3)
+
+num1 = num2
+num2 = num3
+num3 = num1
+
+print("Your current integers are: ", "\nFirst number: ", num1, "\nSecond number: ", num2, "\nThird number: ", num3)
