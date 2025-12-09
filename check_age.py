1b13aec3e2684efe88efb919203da95da2c10061
coderkon7 <jaydentiger77@gmail.com>
2025-12-09 09:30:56 -0600

    Commit

---
+++ check_age.py
@@ -0,0 +1,7 @@
+age = int(input("Enter your age: "))
+if age >=10 and age <=20:
+    print("You are between 10 and 20 years old.")
+elif age <10 or age >20:
+    print("You are not between 10 and 20 years old.")
+else:
+    print("Invalid input")
