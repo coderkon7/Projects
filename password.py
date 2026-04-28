bef1628f5bdc0cdaea26d33f48dc704c4bda7bc2
coderkon7 <jaydentiger77@gmail.com>
2026-04-28 10:44:49 -0500

    Commit

--- password.py
+++
@@ -1,8 +0,0 @@
-print("Welcome to My Fun Mom Password Game")
-password = "hikergirl"
-guess = ""
-while guess != "hikergirl":
-    guess = input("please try and guess the password: ")
-    if guess != "hikergirl":
-        print("Oh dear you didn't get it right. No supper for you")
-print("You win! I'll make you supper tomorrow!")
--- wpm_calculator.py
+++
@@ -1,8 +0,0 @@
-words = float (input ("how many words did you type: "))
-time = float (input ("how many minutes did it take you to type them: "))
-
-def typing_speed (words, time):
-    answer= words / time
-    return answer
-
-print ("Your typing speed is: ", typing_speed(words, time), "words per minute")
