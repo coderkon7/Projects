c97c3b4e19bec2119b3df8702bdb75a865f6304b
coderkon7 <jaydentiger77@gmail.com>
2026-05-12 16:59:35 -0500

    Updated

--- random_password_generator.py
+++ random_password_generator.py
@@ -1,7 +1,7 @@
 import random
 password = ""
-alphabet = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
-numbers = ['1','2','3','4','5','6','7','8','9']
+alphabet = ('a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z')
+numbers = ('1','2','3','4','5','6','7','8','9')
 
 print("Welcome to my password generator!")
 print("This will generate a password with both uppercase and lowercase letters, along with numbers. You can also choose the length of the generated password.")
