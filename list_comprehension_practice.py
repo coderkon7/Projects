aab75543d2d0857e1f17276ebecf55943be05266
coderkon7 <jaydentiger77@gmail.com>
2026-05-26 16:13:19 -0500

    Commit

--- list_comprehension_practice.py
+++ list_comprehension_practice.py
@@ -1,8 +1,15 @@
 num = int(input("Enter a number: "))
 
-list = [num]
+odd_numbers = [i for i in range(1, num) if i % 2 != 0]
+
+print("\nOdd numbers below", num, ":")
+print(odd_numbers)
 
 
 
-fruits = ["apples", "pears", "grapes", "strawberries"]
+fruits = input("\nEnter fruit names separated by space: ").split()
 
+updated_fruits = [fruit.capitalize() for fruit in fruits]
+
+print("\nUpdated Fruit List: ")
+print(updated_fruits)
