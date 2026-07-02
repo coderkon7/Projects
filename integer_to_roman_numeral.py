d47e04a6f9043d2a1fae8ad9198d8fd7d1697a11
coderkon7 <jaydentiger77@gmail.com>
2026-07-02 10:04:01 -0500

    Commit

---
+++ integer_to_roman_numeral.py
@@ -0,0 +1,28 @@
+class RomanNumeral():
+    def __init__(self, num):
+        self.num = num
+    
+    # -------------------
+    # I tried to manually code the number to roman numeral conversion, but I couldn't figure it out, so I copied this code from this website: https://www.geeksforgeeks.org/python/python-program-to-convert-integer-to-roman/
+    def convert(self):
+        num = [1, 4, 5, 9, 10, 40, 50, 90,
+            100, 400, 500, 900, 1000]
+        sym = ["I", "IV", "V", "IX", "X", "XL",
+            "L", "XC", "C", "CD", "D", "CM", "M"]
+        i = 12
+        
+        while self.num:
+            div = self.num // num[i]
+            self.num %= num[i]
+
+            while div:
+                print(sym[i], end = "")
+                div -= 1
+            i -= 1
+        return self.num
+     # -------------------           
+        
+
+num = int(input("Enter the number you want to convert to Roman numerals: "))
+Object = RomanNumeral(num)
+Object.convert()
